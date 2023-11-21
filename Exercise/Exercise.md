353507be18d0280ce9d57f1191e0d6f9a961c726
CarlosCobParro <antonio.cob@edu.uah.es>
2023-11-21 17:45:36 +0100

    Improve docker file to deploy prometheus

--- Exercise/Exercise.md
+++ Exercise/Exercise.md
@@ -2,6 +2,8 @@
 
 In order to work in different environments, the most part of the exercises have been developing using docker and docker compose technologies. The idea is to run this examples in any sort of computer with careless the Operative system.
 
+I recommend to clean the docker background of the machine. To clean the machine of docker image and running/stopping containers I recommend using this [file](./clean-docker.sh) to remove docker trash.
+
 ## [1-a](./1-a/)
 
 In this practice, the idea is to deploy a prometheus server using docker and measure a set of metrics and compare the different types of method to measure. 
---
+++ Exercise/clean-docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+###########################################
+#
+# Simple Shell script to clean/remove all container/images
+#
+# The script will 
+#  - first stop all running containers (if any),
+#  - remove containers
+#  - remove images
+#  - remove volumes
+#
+
+# stop all running containers
+echo '####################################################'
+echo 'Stopping running containers (if available)...'
+echo '####################################################'
+sudo docker stop $(sudo docker ps -aq)
+
+# remove all stopped containers
+echo '####################################################'
+echo 'Removing containers ..'
+echo '####################################################'
+sudo docker rm $(sudo docker ps -aq)
+
+
+# remove all images
+echo '####################################################'
+echo 'Removing images ...'
+echo '####################################################'
+sudo docker rmi -f $(sudo docker images -aq)
+
+# remove all stray volumes if any
+echo '####################################################'
+echo 'Revoming docker container volumes (if any)'
+echo '####################################################'
+sudo docker volume rm $(sudo docker volume ls -q)
